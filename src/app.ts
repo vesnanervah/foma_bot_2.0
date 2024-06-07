bd94e662666ce0753550b3028a275894666863d0
Kirill Zverev <vesnanervakh@gmail.com>
2024-06-07 15:50:20 +0300

    refactor: move out app and membersLocalStorage to separete classes

---
+++ src/app.ts
@@ -0,0 +1,87 @@
+import { Telegraf } from "telegraf";
+import { GEOCODER_KEY, TG_TOKEN, WEATHER_KEY } from "../token.js";
+import { Geocoder } from "./geocoder/geocoder.js";
+import { WeatherClient } from "./weatherClient/weatherClient.js";
+import { whoCommand } from "./simpleCommands/whoCommand.js";
+import { message } from "telegraf/filters";
+import { MembersLocalStorage } from "./membersLocalStorage/collectedMembers.js";
+
+class App {
+    private isResponsing = false;
+    private geocoder = new Geocoder(GEOCODER_KEY);
+    private weatherClient = new WeatherClient(WEATHER_KEY);
+    private bot = new Telegraf(TG_TOKEN);
+    private membersLocalStorage = new MembersLocalStorage();
+    private commands:Commands = {
+        'кто': (commandArgument?: string, members?: Array<string>) => whoCommand(members!),
+        'координаты': (commandArgument?: string) => this.getCityCoordinates(commandArgument),
+        'погода': (commandArgument?: string) => this.getCurrentWeather(commandArgument),
+        'очистить_мемберов': () => this.clearColletedMembers(),
+    };
+
+    startApp() {
+        this.addTextSubscribtion();
+        this.bot.launch();
+        setInterval(() => console.log('Bot is online'), 100000);
+    }
+
+    private addTextSubscribtion() {
+        this.bot.on(message('text'), async (ctx) => {
+            var sender = [ctx.update.message.from.first_name, (ctx.update.message.from.last_name ?? '')].join(' ').trim();
+            this.membersLocalStorage.addMember(sender);
+    
+            if(!ctx.message.text.toLowerCase().startsWith('фома, ')) {
+                return;
+            }
+            if(this.isResponsing) {
+                return;
+            }
+            this.isResponsing = true;
+            var command = ctx.message.text.slice(ctx.message.text.indexOf(',') + 1).trim();
+            var commandName = command.split(' ')[0].toLowerCase();
+            var commandArgument = command.split(' ').slice(1).join(' ');
+            console.log('Incoming command: ' + commandName)
+            console.log('Incoming argument: ' + commandArgument)
+            if (!commandName || commandName.length == 0 || !this.commands[commandName.toLowerCase()]) {
+                ctx.reply('Не понял');
+            } else {
+                var result = await this.commands[commandName.toLowerCase()](commandArgument, this.membersLocalStorage.collectedMembers);
+                if (typeof result === 'string') {
+                    ctx.reply(result);
+                }
+            }
+            this.isResponsing = false;
+          });
+    }
+
+    private clearColletedMembers() {
+        this.membersLocalStorage.clearCollectedMembers();
+        return 'Собранные мемберы почищены...'
+    }
+
+    private async  getCityCoordinates(cityName?: string): Promise<string> {
+        if(!cityName || cityName.length === 0) {
+            return 'А город я угадать должен?';
+        }
+        var response = await this.geocoder.getCityCoordinates(cityName);
+        return response.success ? `Координаты места ${cityName}: широта ${response.latitude}, долгота ${response.longitude}` : (response.errorMessage ?? 'Незахендленный ерор. Еблан керик хуйни накодил.');
+    }
+    
+    private async  getCurrentWeather(cityName?: string): Promise<string> {
+        if(!cityName || cityName.length === 0) {
+            return 'где именно то'
+        }
+        var geocodingResult = await this.geocoder.getCityCoordinates(cityName);
+        if (!geocodingResult.success ) {
+            return geocodingResult.errorMessage ??  'Незахендленный ерор. Еблан керик хуйни накодил.';
+        }
+        var currentWeather = await this.weatherClient.getCurrentWeather(geocodingResult, cityName);
+        return currentWeather;
+    }
+}
+
+type Commands = {
+    [index: string]:  (commandArgument?: string, members?: Array<string>) => any; 
+}
+
+export { App };
---
+++ src/membersLocalStorage/collectedMembers.ts
@@ -0,0 +1,33 @@
+import { LocalStorage } from "node-localstorage";
+
+class MembersLocalStorage {
+    collectedMembersLocalStorageKey = 'members';
+    localStorage = new LocalStorage('./scratch');
+    collectedMembers = this.getLocalCollectedMembers();
+
+    addMember(member:string) {
+        if (member && !this.collectedMembers.includes(member)) {
+            // Костыль сбор всех участников тк в api бота мы не можем получить список участников
+            this.collectedMembers.push(member);
+            localStorage.setItem(this.collectedMembersLocalStorageKey, JSON.stringify(this.collectedMembers));
+            console.log(this.collectedMembers);
+        }
+    }
+
+
+    clearCollectedMembers() {
+        this.localStorage.removeItem(this.collectedMembersLocalStorageKey);
+        this.collectedMembers = [];
+    }
+
+    private getLocalCollectedMembers(): Array<string>{
+        var savedMembers = localStorage.getItem(this.collectedMembersLocalStorageKey);
+        if (savedMembers) {
+            console.log('Got members from local');
+            return JSON.parse(savedMembers);
+        }
+        return new Array<string>;
+    }
+}
+
+export { MembersLocalStorage  };

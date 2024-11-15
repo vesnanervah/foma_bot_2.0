89989dcf514c19e86f8523e11803c18fe2a473b2
Kirill Zverev <kirill.zverev@whitetigersoft.ru>
2024-11-15 11:46:31 +0300

    feat: replace Math.random with Random(ARC4RNG).next because the default one is generating same numbers often

--- src/clients/baseCommandClient.ts
+++ src/clients/baseCommandClient.ts
@@ -1,14 +1,16 @@
 import { Update } from "@telegraf/types";
+import { ARC4RNG, Random } from "random";
 import { Context, NarrowedContext } from "telegraf";
 
 abstract class BaseCommandClient {
+    static random = new Random(new ARC4RNG);
 
     abstract getReply(args: GetReplyArgs):void;
 
     abstract isMatch(commandName: string, commandArgument?: string): boolean;
 
     getRandomValueFromArray<T>( arr:Array<T> ): T {
-        return arr[Math.floor(Math.random() * arr.length)];
+        return arr[Math.floor(BaseCommandClient.random.next() * arr.length)];
     }
 }
 
